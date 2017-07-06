616253d258c66f2f2d8fa8fe103f207aa7e798f3
helenanic <helenanicolodelli@gmail.com>
2017-07-06 18:08:26 -0300

    teste
    teste

---
+++ app/App.js
@@ -0,0 +1,5 @@
+import React from 'react'; // importa a lib react
+import ReactDOM from 'react-dom';// importa a lib react-dom
+import MeuComponente from './components/MeuComponente.js';
+
+ReactDOM.render(<MeuComponente/>, document.querySelector('.app') );
